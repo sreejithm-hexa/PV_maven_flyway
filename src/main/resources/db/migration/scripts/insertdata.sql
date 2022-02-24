6f8b3c6f32127b31a5aabe7a4c9f29a1af2b28c9
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2022-02-24 16:41:29 +0530

    Create insertdata.sql

---
+++ src/main/resources/db/migration/scripts/insertdata.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset author:john splitStatements:true endDelimiter:;
+
+
+INSERT INTO CUSTOMER VALUES('kkrs@gmail.com', 'kkrs', 'kaustiv','MALE', 7278073227,'west bengal', 'kolkata', 5000);
